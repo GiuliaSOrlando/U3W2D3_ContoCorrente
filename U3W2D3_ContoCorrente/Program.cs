7dcba69d34cb7cfca6bd6d231196a98166886ca7
GiuliaSOrlando <134381769+GiuliaSOrlando@users.noreply.github.com>
2023-09-06 15:01:32 +0300

    Update Program.cs (creo conti iniziali)

--- U3W2D3_ContoCorrente/Program.cs
+++ U3W2D3_ContoCorrente/Program.cs
@@ -11,7 +11,20 @@ namespace U3W2D3_ContoCorrente
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            new Banca().MenuBanca();
+            Banca banca = new Banca();
+
+        for (int i = 1; i <= 10; i++)
+            {
+                string nome = $"Nome{i}";
+                string cognome = $"Cognome{i}";
+                DateTime dataApertura = DateTime.Now;
+                string numeroConto = $"IT000000000{i}";
+                decimal saldoIniziale = 1000.0m * i;
+
+                ContoCorrente conto = new ContoCorrente(nome, cognome, dataApertura, numeroConto, saldoIniziale);
+            }
+
+            banca.MenuBanca();
         }
     }
 }
